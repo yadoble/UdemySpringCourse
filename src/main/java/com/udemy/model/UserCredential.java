6756900235af21548a960f044646798504d76511
jacg <jacg@mx1.ibm.com>
2017-07-07 17:41:08 -0500

    first commit

---
+++ src/main/java/com/udemy/model/UserCredential.java
@@ -0,0 +1,38 @@
+package com.udemy.model;
+
+public class UserCredential {
+
+	private String username;
+	private String password;
+
+	/**
+	 * 
+	 */
+	public UserCredential() {}
+
+	/**
+	 * @param username
+	 * @param password
+	 */
+	public UserCredential(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
--- src/main/java/com/udemy/service/PrintLog.java
+++
@@ -1,7 +0,0 @@
-package com.udemy.service;
-
-public interface PrintLog {
-
-	public abstract String message();
-	
-}
